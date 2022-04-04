dfef103553624656df35e6ce49e2625f9472c970
Opilane TTHK <opilane@tthk.ee>
2022-04-04 13:56:10 +0300

    (Video 12) Teha kollektsioonide kasutamine

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,13 +3,39 @@
 namespace App\Models;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 class Post
 {
+    public $title;
+    public $excerpt;
+    public $date;
+    public $body;
+    public $slug;
+
+    public function __construct($title, $excerpt, $date, $body,$slug)
+    {
+        $this->title = $title;
+        $this->excerpt = $excerpt;
+        $this->date = $date;
+        $this->body = $body;
+        $this->slug = $slug;
+    }
+
     public static function all()
     {
         $files = File::files(resource_path("posts/"));
-        return array_map(fn($file)=>$file->getContents(),$files);
+        $posts=[];
+        foreach ($files as $file){
+            $document = YamlFrontMatter::parseFile($file);
+            $posts[] = new Post(
+                $document->title,
+                $document->excerpt,
+                $document->date,
+                $document->body(),
+            );
+        }
+        return $posts;
     }
 
     public static function find($slug)
