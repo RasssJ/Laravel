76502fec6e3e452b3867f63f0465346cb0fc2d73
Opilane TTHK <opilane@tthk.ee>
2022-04-04 14:17:20 +0300

    Add currentcategory to dropdown in categories view

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('posts', [
-        "posts"=>Post::latest()->with(["category","author"])->get()
+        "posts"=>Post::latest()->with(["category","author"])->get(),
+        "categories"=>Category::all()
     ]);
 });
 Route::get('/posts/{post:slug}', function (Post $post) {
@@ -30,12 +31,15 @@ Route::get('/posts/{post:slug}', function (Post $post) {
 });
 Route::get('/categories/{category:slug}', function (Category $category) {
     return view('posts', [
-        "posts"=> $category->posts->load(["category","author"])
+        "posts"=> $category->posts->load(["category","author"]),
+        "currentCategory"=>$category,
+        "categories"=>Category::all()
     ]);
 });
 Route::get('/authors/{author:username}', function (User $author) {
     return view('posts', [
-        "posts"=> $author->posts->load(["category","author"])
+        "posts"=> $author->posts->load(["category","author"]),
+        "categories"=>Category::all()
     ]);
 });
 
