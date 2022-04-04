b6047c5bfddb8af0c46fddabacf5860ae61b1877
Opilane TTHK <opilane@tthk.ee>
2022-04-04 14:19:02 +0300

    Make dropdown into components

--- routes/web.php
+++ routes/web.php
@@ -23,19 +23,19 @@ Route::get('/', function () {
         "posts"=>Post::latest()->with(["category","author"])->get(),
         "categories"=>Category::all()
     ]);
-});
+})->name("home");
 Route::get('/posts/{post:slug}', function (Post $post) {
     return view('post', [
         "post"=> $post
     ]);
-});
+})->name("post");
 Route::get('/categories/{category:slug}', function (Category $category) {
     return view('posts', [
         "posts"=> $category->posts->load(["category","author"]),
         "currentCategory"=>$category,
         "categories"=>Category::all()
     ]);
-});
+})->name("category");
 Route::get('/authors/{author:username}', function (User $author) {
     return view('posts', [
         "posts"=> $author->posts->load(["category","author"]),
