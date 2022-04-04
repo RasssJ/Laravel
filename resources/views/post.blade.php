e68a4ee2e3cb53aafaced845e96a522074bc0f28
Opilane TTHK <opilane@tthk.ee>
2022-04-04 14:12:03 +0300

    Add authors post viewing capability

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,7 +1,12 @@
 <x-layout>
     <article>
         <h1>{{ $post->title }}</h1>
-        <div>
+        <p> By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a>
+            in <a href="/categories/{{ $post->category->slug }}">
+                {{ $post->category->name }}
+            </a>
+        </p>
+        <div style="margin: 5px 0">
             {!! $post->body !!}
         </div>
     </article>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,9 +7,11 @@
             </a>
         </h1>
         <div style="background-color: lightblue;margin-bottom:5px">
-            <a href="/categories/{{ $post->category->slug }}">
-                {{ $post->category->name }}
-            </a>
+            <p> By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a>
+                in <a href="/categories/{{ $post->category->slug }}">
+                    {{ $post->category->name }}
+                </a>
+            </p>
         </div>
         <div>
             {!! $post->excerpt !!}
